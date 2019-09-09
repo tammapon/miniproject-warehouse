7c9efcf71baaf6fff710fed73dba43d0c7d89a16
klui_tammapon.N <kluy2000@gmail.com>
2019-09-09 19:49:51 +0700

    Add files via upload

---
+++ TruckClass.py
@@ -0,0 +1,7 @@
+class truck:
+    def __init__(self,name="",minload=0,maxload=0):
+        self.name=name
+        self.minload=minload
+        self.maxload=maxload
+        self.distance=0
+        self.lastplacename=''
